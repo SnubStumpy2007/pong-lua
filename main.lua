27953f79b4ffa26ec8e9f44202c23dedeec20724
SnubStumpy2007 <joshua192007@gmail.com>
2024-10-02 17:36:19 -0400

    update

--- main.lua
+++ main.lua
@@ -1,4 +1,13 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+
 function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+      })
+
 end
 
 
