038c727b3d330a55c87afd8f25cd98ffc23ee77b
SnubStumpy2007 <joshua192007@gmail.com>
2024-10-03 18:32:07 -0400

    adding initial comments

--- main.lua
+++ main.lua
@@ -1,10 +1,28 @@
 function love.load()
+-- importing libraries
+
+-- defining player paddles
+
+
+
+-- animations
+
+
 
 end
 
 
 function love.update(dt)
+
+  -- keyboard movement
+
+  -- computer movement
+
+
+  -- collision physics
+
 end
 
 function love.draw()
+  -- camera
 end
