f987c27f555d8cec6061c19c84128ea82f3a083b
SnubStumpy2007 <joshua192007@gmail.com>
2024-10-03 18:00:29 -0400

    setting up main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,4 @@
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
-
-
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        resizable = false,
-      })
 
 end
 
