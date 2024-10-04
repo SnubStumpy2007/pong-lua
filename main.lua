16ef0d74f832e2c3a9d1ede743c6affe60f94a5d
SnubStumpy2007 <joshua192007@gmail.com>
2024-10-04 16:54:59 -0400

    update

--- main.lua
+++ main.lua
@@ -6,8 +6,16 @@ function love.load()
     background = love.graphics.newImage('maps/fancy-court.png')
 
     -- defining player paddles
-    p1Paddle = love.graphics.newImage('sprites/fancy-paddle-blue.png')
-    cpuPaddle = love.graphics.newImage('sprites/fancy-paddle-green.png')
+    -- p1Paddle = love.graphics.newImage('sprites/fancy-paddle-blue.png')
+    -- cpuPaddle = love.graphics.newImage('sprites/fancy-paddle-green.png')
+    p1paddle = {}
+    p1paddle.x = 400
+    p1paddle.y = 200
+    p1paddle.speed = 300
+    p1paddle.sprite = love.graphics.newImage('sprites/fancy-paddle-blue.png')
+    p1paddle.grid = anim8.newGrid(32, 128, p1paddle.sprite:getWidth(), p1paddle.sprite:getHeight())
+
+
     ball = love.graphics.newImage('sprites/fancy-ball.png')
 
     -- starting player scores
@@ -15,7 +23,8 @@ function love.load()
     cpuScore = {}
 
 -- animations
-
+  p1animations = {}
+  p1animations.down = anim8.newAnimation(p1paddle.grid('1-4', 1), 0.2)
 
 
 end
@@ -35,9 +44,9 @@ end
 function love.draw()
 
   -- background
-  love.graphics.draw(background, 0,0)
-  love.graphics.draw(p1Paddle, 0,250)
-  love.graphics.draw(cpuPaddle, 766, 250)
+  -- love.graphics.draw(background, 0,0)
+  -- love.graphics.draw(p1Paddle, 0,250)
+  -- love.graphics.draw(cpuPaddle, 766, 250)
   love.graphics.draw(ball, 385, 300)
   -- camera
 end
